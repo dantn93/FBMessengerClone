0cfa2cd5e66a6d0f231cafab3010b8bdffa73e08
macos <macos@macoss-MacBook-Pro.local>
2018-07-27 11:34:31 +0700

    how to install react native app

---
+++ Facebook-Messenger-Clone/README.js
@@ -0,0 +1,3 @@
+// How to use:
+// 1. First, run command `./run.sh install` to install something
+// 2. Start app with command `./run.sh ios` for ios simulator or `./run.sh android` for androdi simulator
