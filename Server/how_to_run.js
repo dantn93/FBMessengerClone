de6bcfbb720965ed008e7cb5208ef274242887f3
macos <macos@macoss-MacBook-Pro.local>
2018-07-31 19:09:32 +0700

    how to use

---
+++ Server/how_to_run.js
@@ -0,0 +1,23 @@
+/* == START THE SERVER ==
+1. Go to folder "Server"
+execute the command: "yarn start"
+*/
+
+/* == START CLIENTS ==
+1. Go to folder "Facebook-Messenger-Clone"
+2. Execute the command: "./run.sh install" to install and link libraries
+*/
+
+/*
+Please use follow users for running app:
+heavycat176         -> name: emilia 
+heavysnake748       -> name: denny
+organiccat819       -> name: birol
+yellowostrich532    -> name: sieglinde
+happyzebra486       -> name: marie-claude
+bluepanda145        -> name: encarnacion
+bluefish979         -> name:
+beautifulduck197    -> name: arend-jan
+biggoose900         -> name: hugo
+browntiger427       -> name: rocio
+*/
--- Server/index.js
+++ Server/index.js
@@ -3,84 +3,93 @@ const bodyParser = require('body-parser');
 const Chatkit = require('@pusher/chatkit-server');
 const [CHATKIT_INSTANCE_LOCATOR, SECRET_KEY] = require('./config');
 
+const chatkit = new Chatkit.default({
+  instanceLocator: CHATKIT_INSTANCE_LOCATOR,
+  key: SECRET_KEY,
+});
 
 const app = express(); // (2)
 app.use(bodyParser.json());
 
 
-function createUser(chatkit, id, name, avatarURL, res){
-    chatkit.createUser({
-        id, name, avatarURL
-    })
+function createUser(chatkit, id, name, avatarURL, res) {
+  chatkit.createUser({
+    id, name, avatarURL
+  })
     .then(() => {
-        console.log('Create user successfully');
-        res.status(200).send('Create user successfully');
+      console.log('Create user successfully');
+      res.status(200).send('Create user successfully');
     }).catch((err) => {
-        console.log('Unable to create user');
-        res.status(404).send('Unable to create user');
+      console.log('Unable to create user');
+      res.status(404).send('Unable to create user');
     });
 }
-app.post('/create/user', function(req, res) { // (5)
-    const name = req.body.name;
-    const id = req.body.id;
-    const avatarURL = req.body.avatar;
+app.post('/create/user', function (req, res) { // (5)
+  const name = req.body.name;
+  const id = req.body.id;
+  const avatarURL = req.body.avatar;
 
-    console.log(name);
-    console.log(id);
-    console.log(avatarURL);
+  console.log(name);
+  console.log(id);
+  console.log(avatarURL);
 
-    const chatkit = new Chatkit.default({
-        instanceLocator: CHATKIT_INSTANCE_LOCATOR,
-        key: SECRET_KEY,
-    });
-
-    //check user exist
-    chatkit.getUsers()
+  //check user exist
+  chatkit.getUsers()
     .then((users) => {
-        const user = users.filter(user => user.name == name);
-        if(user.length == 0){
-            createUser(chatkit, id, name, avatarURL, res);
-        }else{
-            console.log('User exist');
-            res.status(200).send('User exist');
-        }
+      const user = users.filter(user => user.name == name);
+      if (user.length == 0) {
+        createUser(chatkit, id, name, avatarURL, res);
+      } else {
+        console.log('User exist');
+        res.status(200).send('User exist');
+      }
     }).catch((err) => {
-        console.log('Cant get user from Chatkit');
-        res.status(404).send('Cant get user from Chatkit');
+      console.log('Cant get user from Chatkit');
+      res.status(404).send('Cant get user from Chatkit');
     });
 });
 
-app.post('/check/user', function(req, res){
-    const name = req.body.name;
-    const id = req.body.id;
-    const avatarURL = req.body.avatar
+app.post('/check/user', function (req, res) {
+  const name = req.body.name;
+  const id = req.body.id;
+  const avatarURL = req.body.avatar;
 
-    const chatkit = new Chatkit.default({
-        instanceLocator: CHATKIT_INSTANCE_LOCATOR,
-        key: SECRET_KEY,
-    });
-
-    //check user exist
-    chatkit.getUsers()
+  //check user exist
+  chatkit.getUsers()
     .then((users) => {
-        const user = users.filter(user => user.name == name);
-        if(user.length == 0){
-            console.log('User is not found');
-            res.status(202).send('User is not found');
-        }else{
-            console.log('Check user exist');
-            res.status(200).send('Check user exist');
-        }
+      const user = users.filter(user => user.name == name);
+      if (user.length == 0) {
+        console.log('User is not found');
+        res.status(202).send('User is not found');
+      } else {
+        console.log('Check user exist');
+        res.status(200).send('Check user exist');
+      }
     }).catch((err) => {
-        console.log('Cant get user from Chatkit');
-        res.status(404).send('Cant get user from Chatkit');
+      console.log('Cant get user from Chatkit');
+      res.status(404).send('Cant get user from Chatkit');
     });
 });
 
-app.post('/get/user', function(req, res){
-    
+app.post('/get/user', function (req, res) {
+  const name = req.body.name;
+  //check user exist
+  chatkit.getUsers()
+    .then((users) => {
+      const user = users.filter(user => user.name == name);
+      if (user.length == 0) {
+        console.log('User is not found');
+        res.status(202).send('User is not found');
+      } else {
+        console.log('Check user exist');
+        res.status(200).send(user);
+      }
+    }).catch((err) => {
+      console.log('Cant get user from Chatkit');
+      res.status(404).send('Cant get user from Chatkit');
+    });
 });
 
-app.listen(4000, function() { // (6)
-    console.log('App listening on port 4000');
+app.listen(4000, function () { // (6)
+  console.log('App listening on port 4000');
 });
