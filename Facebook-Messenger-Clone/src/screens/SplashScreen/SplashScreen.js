6ebe1d0a6ec78d6b06066cce58193656efb3a0b2
macos <macos@macoss-MacBook-Pro.local>
2018-08-07 10:02:09 +0700

    splash screen: account kit layout

--- Facebook-Messenger-Clone/src/screens/SplashScreen/SplashScreen.js
+++ Facebook-Messenger-Clone/src/screens/SplashScreen/SplashScreen.js
@@ -1,93 +1,181 @@
 import React, { Component } from 'react';
-import { View, TextInput, AsyncStorage, Text } from 'react-native';
+import { View, TextInput, AsyncStorage, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Button } from 'react-native-paper';
 import { connect } from 'react-redux';
 import { users } from '@assets/fake_data';
 import { SECRET_KEY, CHATKIT_TOKEN_PROVIDER_ENDPOINT, CHATKIT_INSTANCE_LOCATOR } from '@config/chatConfig';
 import axios from 'axios';
-import styles from './styles';
-
+import RNAccountKit, { LoginButton, Color, StatusBarStyle } from 'react-native-facebook-account-kit';
 
 class SplashScreen extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { username: '', avatar: '', continue: false };
-      }
+    state = {
+        authToken: null,
+        loggedAccount: null,
+    }
+    componentWillMount() {
+        this.configureAccountKit()
 
-    // componentDidMount(){
-    //     console.log(users.results.slice(0, 10));
-    // }
-    // goToMainScreen(){
-    //     const { navigation } = this.props;
-    //     navigation.navigate('MainScreen');
-    // }
-    // onCreateUser = async () => {
-    //     try{
-    //         const user = users.results.slice(0, 10).filter(user => user.login.username == this.state.username)[0];
-    //         if(user != null){
-    //             //create this user in pusher server
-    //             console.log('Have user');
-    //             const flag = await this.postCreateUser(user);
-    //             if(await this.storeData(user)){
-    //                 this.setState({continue: true});
-    //             }
-    //         }
-    //         return false;
-    //     }catch(e){
-    //         return false;
-    //         console.log(e);
-    //     }
-    // }
-    // storeData = async (user) => {
-    //     try {
-    //         await AsyncStorage.setItem('username', this.state.username);
-    //         await AsyncStorage.setItem('avatar', user.picture.thumbnail);
-    //         return true;
-    //     } catch (error) {
-    //         console.log('Cannot save user into AsyncStorage');
-    //         return false;
-    //     }
-    // }
+        RNAccountKit.getCurrentAccessToken()
+            .then(token => {
+                if (token) {
+                    RNAccountKit.getCurrentAccount().then(account => {
+                        this.setState({
+                            authToken: token,
+                            loggedAccount: account,
+                        })
+                    })
+                } else {
+                    console.log('No user account logged')
+                }
+            })
+            .catch(e => console.log('Failed to get current access token', e))
+    }
 
-    // postCreateUser = (user) => {
-    //     axios.post('http://localhost:4000/create/user', {
-    //         "name": this.state.username,"id": this.state.username, "avatar": user.picture.thumbnail
-    //         })
-    //         .then(function (response) {
-    //             if(response.status == 200){
-    //                 console.log('Create user successfully');
-    //             }
-    //         })
-    //         .catch(function (error) {
-    //             console.log('Cant create user');
-    //         });
-    // }
+    configureAccountKit() {
+        RNAccountKit.configure({
+            theme: {
+                //backgroundColor:       Color.rgba(0,120,0,0.1),
+                //buttonBackgroundColor: Color.rgba(0, 153, 0, 1.00),
+                //buttonDisabledBackgroundColor: Color.rgba(100, 153, 0, 0.5),
+                //buttonBorderColor:     Color.rgba(0,255,0,1),
+                //buttonTextColor:       Color.rgba(0,255,0,1),
+                //headerBackgroundColor: Color.rgba(0, 153, 0, 1.00),
+                //headerTextColor:       Color.rgba(0,255,0,1),
+                //headerButtonTextColor: Color.rgba(0,255,0,1),
+                //iconColor:             Color.rgba(0,255,0,1),
+                //inputBackgroundColor:  Color.rgba(0,255,0,1),
+                //inputBorderColor:      Color.hex('#ccc'),
+                //inputTextColor:        Color.hex('#0f0'),
+                //textColor:             Color.hex('#0f0'),
+                //titleColor:            Color.hex('#0f0'),
+                //backgroundImage:       "background.png",
+                //statusBarStyle:        StatusBarStyle.LightContent,
+            },
+            //countryWhitelist: [ "AR", "BR", "US" ],
+            //countryBlacklist: [ "BR" ],
+            //defaultCountry: "AR"
+            initialEmail: 'mocnhantrang@gmail.com',
+            initialPhoneCountryPrefix: '+84',
+            initialPhoneNumber: '1292849917',
+        })
+    }
+
+    onLogin(token) {
+        if (!token) {
+            console.warn('User canceled login')
+            this.setState({})
+        } else {
+            RNAccountKit.getCurrentAccount().then(account => {
+                this.setState({
+                    authToken: token,
+                    loggedAccount: account,
+                })
+            })
+        }
+    }
+
+    onLoginError(e) {
+        console.log('Failed to login', e)
+    }
+
+    onEmailLoginPressed() {
+        RNAccountKit.loginWithEmail()
+            .then(token => {
+                this.onLogin(token)
+            })
+            .catch(e => this.onLoginError(e))
+    }
+
+    onLogoutPressed() {
+        RNAccountKit.logout()
+            .then(() => {
+                this.setState({
+                    authToken: null,
+                    loggedAccount: null,
+                })
+            })
+            .catch(e => console.log('Failed to logout'))
+    }
+
+    renderUserLogged() {
+        const { id, email, phoneNumber } = this.state.loggedAccount
+
+        return (
+            <View>
+                <TouchableOpacity style={styles.button} onPress={() => this.onLogoutPressed()}>
+                    <Text style={styles.buttonText}>Logout</Text>
+                </TouchableOpacity>
+
+                <Text style={styles.label}>Account Kit Id</Text>
+                <Text style={styles.text}>{id}</Text>
+                <Text style={styles.label}>Email</Text>
+                <Text style={styles.text}>{email}</Text>
+                <Text style={styles.label}>Phone Number</Text>
+                <Text style={styles.text}>{phoneNumber ? `${phoneNumber.countryCode} ${phoneNumber.number}` : ''}</Text>
+            </View>
+        )
+    }
+
+    renderLogin() {
+        return (
+            <View>
+                <LoginButton
+                    style={styles.button}
+                    type="phone"
+                    onLogin={token => this.onLogin(token)}
+                    onError={e => this.onLogin(e)}
+                >
+                    <Text style={styles.buttonText}>SMS</Text>
+                </LoginButton>
+
+                <TouchableOpacity style={styles.button} onPress={() => this.onEmailLoginPressed()}>
+                    <Text style={styles.buttonText}>Email</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
 
     render() {
         return (
-            <View style={styles.container}>
-                {/* <TextInput
-                    style={{width: 200, height: 40, borderColor: 'gray', borderWidth: 1}}
-                    onChangeText={(username) => this.setState({username})}
-                    value={this.state.username}
-                    placeholder={'username'}
-                    autoCapitalize = 'none'
-                />
-                {this.state.continue == true ? <Button raised color="#0084ff" onPress={() => this.goToMainScreen()}>
-                    <Text>CONTINUE AS USER</Text>
-                </Button> : <View></View>}
-                {this.state.continue == false ? <Button raised color="#0084ff" onPress={this.onCreateUser}>
-                    <Text>CREATE USER</Text>
-                </Button> : <View></View>} */}
-                <Text>Hello</Text>
-            </View>
-        );
+            <View style={styles.container}>{this.state.loggedAccount ? this.renderUserLogged() : this.renderLogin()}</View>
+        )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    button: {
+        height: 50,
+        width: 300,
+        backgroundColor: 'aqua',
+        marginBottom: 10,
+    },
+    buttonText: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    label: {
+        fontSize: 20,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        marginTop: 20,
+    },
+    text: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+})
 
 
 const mapStateToProps = state => ({
     nav: state.nav,
-  });
-  
-  export default connect(mapStateToProps)(SplashScreen)
+});
+
+export default connect(mapStateToProps)(SplashScreen)
